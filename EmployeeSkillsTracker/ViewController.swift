c9e822d7af43cb93feab9f3a3a976d0ec8a6ef00
Ashlesha Kamble <ashlesha.kamble@yash.com>
2022-06-07 13:49:21 +0530

    First commit

--- EmployeeSkillsTracker/ViewController.swift
+++ EmployeeSkillsTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     let sideMenu = ["Home","Create Profile","Logout"]
     var isSideBarOpen = false
+    let variable = "Ashlesha"
     
     override func viewDidLoad() {
         super.viewDidLoad()
